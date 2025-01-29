47167307be6cbe5ec137ada956233e0e62f2122f
Emma Lilliu <elilliu@paul-f4Br9s2.clusters.42paris.fr>
2025-01-29 17:36:25 +0100

    fini l'epilepsie

--- srcs/fill_window.c
+++ srcs/fill_window.c
@@ -6,25 +6,27 @@
 /*   By: elilliu <elilliu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 14:23:29 by elilliu           #+#    #+#             */
-/*   Updated: 2025/01/29 17:04:34 by elilliu          ###   ########.fr       */
+/*   Updated: 2025/01/29 17:32:36 by elilliu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-void	put_red_square(t_data *data, int x, int y, int size)
+void	put_player(t_data *data, int x, int y, int size)
 {
 	int	i;
 	int	j;
 
-	i = 0;
 	j = 0;
 	while (j < size)
 	{
 		i = 0;
 		while (i < size)
 		{
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, x + i, y + j, 0xcd5c5c);
+			if (i >= data->player.x && i <= data->player.x + 8 && j >= data->player.y && j <= data->player.y + 8)
+				mlx_pixel_put(data->mlx_ptr, data->win_ptr, x + i, y + j, 0xcd5c5c);
+			else
+				mlx_pixel_put(data->mlx_ptr, data->win_ptr, x + i, y + j, 0xf8f8ff);
 			i++;
 		}
 		j++;
@@ -36,7 +38,6 @@ void	put_grey_square(t_data *data, int x, int y, int size)
 	int	i;
 	int	j;
 
-	i = 0;
 	j = 0;
 	while (j < size)
 	{
@@ -55,7 +56,6 @@ void	put_white_square(t_data *data, int x, int y, int size)
 	int	i;
 	int	j;
 
-	i = 0;
 	j = 0;
 	while (j < size)
 	{
@@ -86,9 +86,11 @@ int	fill_window(t_data *data)
 		x = 0;
 		while (data->map.tab[row][i])
 		{
+			if (data->map.tab[row][i] == 'N')
+				put_player(data, x, y, 63);
 			if (data->map.tab[row][i] == '1')
 				put_grey_square(data, x, y, 63);
-			else if (data->map.tab[row][i] == '0' || data->map.tab[row][i] == 'N')
+			else if (data->map.tab[row][i] == '0')
 				put_white_square(data, x, y, 63);
 			x += 64;
 			i++;
@@ -96,6 +98,5 @@ int	fill_window(t_data *data)
 		y += 64;
 		row++;
 	}
-	put_red_square(data, data->player.column * 64 + data->player.x, data->player.line * 64 + data->player.y, 8);
 	return (1);
 }
